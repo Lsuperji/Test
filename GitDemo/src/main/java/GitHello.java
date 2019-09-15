3c1d9d19ece165d2e6e6daff1bafd96f24146000
Lsuperji <252622430@qq.com>
2019-09-15 09:21:04 +0800

    二次提交

---
+++ GitDemo/src/main/java/GitHello.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: JianChao-Liu
+ * @Date: 2019/9/15 9:19
+ */
+public class GitHello {
+    public static void main(String[] args) {
+        System.out.println("GitHello");
+        System.out.println("GitHello");
+    }
+}
